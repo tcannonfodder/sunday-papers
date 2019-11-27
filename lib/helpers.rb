f6479fb5ce8004f7cb64a6503d61d3cf0e150134
Thomas Cannon <tcannon00@gmail.com>
2019-11-26 22:50:20 -0500

    Add `publish_time_tag` helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,9 @@
 use_helper Nanoc::Helpers::Blogging
 use_helper Nanoc::Helpers::Capturing
+
+def publish_time_tag(date= nil)
+  date ||= @item[:created_at]
+  <<~HTML
+  <time datetime="#{date}">#{date.strftime("%B, %-d %Y")}</time>
+  HTML
+end
